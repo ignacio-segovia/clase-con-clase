3a2284311a45c2b438cdfddc057d125d6877efa9
Nacho <segoviaignacio98@hotmail.com>
2024-07-16 21:18:16 -0300

    primer commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+class Album {
+    title: string;
+    songs: string[];
+    constructor(title: string, songs: string[]) {
+        this.title = title;
+        this.songs = songs;
+    }
+}
+class Banda {
+    members: string[];
+    albums: Album[];
+    constructor(members: string[], albums: Album[]) {
+        this.members = members;
+        this.albums = albums;
+    }
+}
+function main() {
+    const unAlbum = { 
+    title:"album de marce",
+    songs: ["una cancion"],
+};
+const unaBanda = new Banda(["marce"], [unAlbum, unAlbum, unAlbum]);
+console.log(unaBanda.albums);
+}
+main();
